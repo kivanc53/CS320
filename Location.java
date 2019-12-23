65d76b7036568c4b42c2cb3aa0db864bc986fafb
Kıvanç Yılmaz <58771711+kivanc53@users.noreply.github.com>
2019-12-24 01:48:29 +0300

    Create Location.java

---
+++ Location.java
@@ -0,0 +1,13 @@
+public class Location {
+    String city;
+    String address;
+    int zip;
+
+    public Location(String city, String address, int zip) {
+        this.city = city;
+        this.address = address;
+        this.zip = zip;
+    }
+
+
+}
