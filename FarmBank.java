474349c856e3800299e9578302e3a5e5cee194fa
Gokberk43 <58912380+Gokberk43@users.noreply.github.com>
2019-12-24 17:35:45 +0300

    Create FarmBank.java

---
+++ FarmBank.java
@@ -0,0 +1,57 @@
+/*import java.util.ArrayList;
+
+public class FarmBank {
+	double balance;
+	ArrayList<Customer> customers;
+	ArrayList<Farmer> farmers;
+	ArrayList<User> users;
+	int accno = 1;
+	DataBase db;
+	
+	
+	public FarmBank(double balance , DataBase db) {
+		this.balance = balance;
+		this.customers = new ArrayList<Customer>();
+		this.farmers = new ArrayList<Farmer>();
+		this.users = new ArrayList<User>();
+		this.db = db;
+	}
+	
+	public double getBalance() {
+		return balance;
+		
+	}
+	/*
+	public void Transfer(Customer c1 , Farmer f1 , double x) {
+		c1.acc.balance -= x;
+		f1.acc.balance += (x*98)/100 ;
+		this.balance += (x*2)/100;
+	}
+	
+	public void addFarmer(Farmer farmer) {
+		farmers.add(farmer);
+		(farmer).giveinfo();
+		db.AddUser(farmer.user, farmer.pw);
+		accno += 1;
+		users.add(farmer);
+	}
+	
+	public void addConsumer(Customer consumer) {
+		customers.add(consumer);
+		consumer.giveinfo();
+		db.AddUser(consumer.user, consumer.pw);
+		accno += 1;
+		users.add(consumer);
+	}
+	
+	public User Search(String username) {
+		for (User user : users) {
+			if(user.user.equals(username)) return user;
+		}
+		return users.get(0);
+	}
+	
+	*/
+
+
+
