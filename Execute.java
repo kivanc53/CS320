91fb5e0587f24de4d455e335b1fcab209261fea2
OnurYilmazGit <46044317+OnurYilmazGit@users.noreply.github.com>
2019-12-25 15:07:36 +0300

    Update Execute.java

--- Execute.java
+++ Execute.java
@@ -1,7 +1,17 @@
 public class Execute {
 	public static void main(String[] args) {
-		
-		Gui gui = new Gui();
+
+	Gui gui = new Gui();
+	Product product = new Product("prd1", "dsc", 100, 100);
+	ArrayList<Product> products = new ArrayList<Product>();
+	products.add(product);
+
+	Location location = new Location("loc", "address", 111);
+	ArrayList<Location> locations = new ArrayList<Location>();
+	locations.add(location);
+
+	Account account = new Account(1, 5);
+	Farmer far = new Farmer("f1", "aa", "111", "far1", "aa", account, "aa", "asd", location, locations, products);
 
 	}
 
